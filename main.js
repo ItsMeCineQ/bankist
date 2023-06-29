07c33eb4c41fe15b7ce2ee2757a432ed96362f2e
ItsMeCineQ <131790357+ItsMeCineQ@users.noreply.github.com>
2023-06-29 20:36:01 +0200

    building a slider

--- main.js
+++ main.js
@@ -174,14 +174,11 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function(entries, observer){
     const [entry] = entries;
-    console.log(entries);
 
     if(!entry.isIntersecting) return;
     else entry.target.classList.remove('section--hidden');
     observer.unobserve(entry.target);
 };
-
-
 
 const sectionObserver = new IntersectionObserver(revealSection, {
     root: null,
@@ -190,14 +187,70 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function(section){
     sectionObserver.observe(section);
-    section.classList.add('section--hidden');
+    //section.classList.add('section--hidden');
 });
 
+// Lazy loading images
+const img = document.querySelectorAll('img[data-src]');
+
+const revealImg = function(entries, observer){
+    const [entry] = entries;
+
+    if(!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+    
+    entry.target.addEventListener('load', function(){
+        entry.target.classList.remove('lazy-img');
+    });
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(revealImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+});
+
+img.forEach(function(img){
+    imgObserver.observe(img);
+});
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function(slide){
+    slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`);
+};
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function(){
+    if(curSlide === maxSlide - 1) curSlide = 0;
+    else{
+        curSlide++;
+    };
+    goToSlide(curSlide);
+};
+
+const prevSlide = function(){
+    if(curSlide === 0){
+        curSlide = maxSlide - 1;
+    }
+    else curSlide--;
+    goToSlide(curSlide);
+};
 
 
-
-
-
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 
 
 
